520e347319a9f5d39982f8f05db389263ce318f2
anton-jj <jappinen.anton@protonmail.com>
2024-12-19 08:09:12 +0100

    init

--- JavaGame(merge)/command/HelpCommand.java
+++
@@ -1,8 +0,0 @@
-package command;
-
-public class HelpCommand {
-
-    public void help() {
-        System.out.println("Available commands:\n'Help'\n'Search'\n'Open <Object>'\n'Pickup <key>'\n'Say <answer>'\n'Exit'" );
-    }
-}
